b515c8ff17087787f822a7fde6bb11eac8122577
Nabeel <mnabeel_92@yahoo.com>
2020-12-20 22:03:28 +0500

    Function with array of unlinked users

--- linked.js
+++ linked.js
@@ -1,1 +1,21 @@
-// find all the users that have not been linked
+// find all the users that have not been linked 
+import {linked, allUsers} from './users';
+
+const formatUsers = (users) => {
+    let formattedUsers = {};
+    users.forEach((user) => {
+        formattedUsers[user.id] = user;
+    });
+    return formattedUsers;
+}
+
+const findUnlinkedUsers = (usersList, linkedUsers) => {
+    const users = formatUsers(usersList);
+    
+    linkedUsers.forEach((linkedUser) => {
+        const id = linkedUser[id];
+        delete users[id];
+    })
+}
+
+findUnlinkedUsers(allUsers, linked);
